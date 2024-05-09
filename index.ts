ea19bd4e39e5456b2ff8c23fa1f8127ca832b9e9
Tex <techcenter2001@gmail.com>
2024-05-09 19:47:16 +0300

    finshing comment feature

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
+import { ObjectId } from "mongodb";
 dotenv.config();
 const app = express();
 const corsOptions = {
@@ -89,10 +90,9 @@ const placeSchema = new mongoose.Schema({
 const CommentSchema = new Schema({
   userID: { type: Types.ObjectId, required: true, ref: "User" },
   placeID: { type: Types.ObjectId, required: true, ref: "Place" },
-  rank: Number,
   rate: Number,
   title: String,
-  dateOfWritten: Date,
+  writtenDate: Date,
   commentBody: String,
   dateVisit: Date,
   services: Number,
@@ -107,26 +107,26 @@ const CommentSchema = new Schema({
   score: { type: Number, default: 0 },
 
   // Optional properties for ThingsToDo
-  locationRate: { type: Number, default: 0 }, // Provide defaults if these are optional
-  safety: { type: Number, default: 0 },
-  facilities: { type: Number, default: 0 },
-  convenience: { type: Number, default: 0 },
-  staff: { type: Number, default: 0 },
+  locationRate: { type: Number }, // Provide defaults if these are optional
+  safety: { type: Number },
+  facilities: { type: Number },
+  convenience: { type: Number },
+  staff: { type: Number },
 
   // Optional properties for ThingsToEat
 
-  foodQuality: { type: Number, default: 0 },
-  valueForMoney: { type: Number, default: 0 },
-  service: { type: Number, default: 0 },
-  menuVariety: { type: Number, default: 0 },
-  ambiance: { type: Number, default: 0 },
+  foodQuality: { type: Number },
+  valueForMoney: { type: Number },
+  service: { type: Number },
+  menuVariety: { type: Number },
+  ambiance: { type: Number },
 
   // Optional properties for PlacesToStay
 
   //ambience and location rate also
-  serviceRate: { type: Number, default: 0 },
-  roomQuality: { type: Number, default: 0 },
-  cleanliness: { type: Number, default: 0 }, // Note: this field was already declared, consider renaming if different meanings are intended
+  serviceRate: { type: Number },
+  roomQuality: { type: Number },
+  cleanliness: { type: Number }, // Note: this field was already declared, consider renaming if different meanings are intended
 });
 // images/photo.png
 
@@ -144,7 +144,7 @@ const UserSchema = new mongoose.Schema({
   userName: { type: String, default: "New User" },
   password: String, // Passwords should always be provided explicitly and hashed
   joinDate: { type: Date, default: Date.now },
-  avatarImage: { type: String, default: "avatarImage.jpg" }, // Automatically set the join date to the current date
+  avatarImage: { type: String, default: "avatars/avatarImage.jpg" }, // Automatically set the join date to the current date
   rank: { type: String, default: "Explorer" }, // Default user ranks Explorer, Adventurer, Trailblazer
   points: { type: Number, default: 0 }, // Start points at 0
   email: String, // Email should be provided explicitly if required
@@ -158,6 +158,7 @@ const UserSchema = new mongoose.Schema({
   photosReview: { type: [Types.ObjectId], default: [] }, // Default to an empty array for photo reviews
   trips: { type: [Types.ObjectId], default: [] }, // Default to an empty array of trips
   runningTrip: { type: String, default: "No active trip" }, // Default to 'No active trip' if none is running
+  rankImage: { type: String, default: "ranks/rankImagelevelThree.jpg" },
 });
 
 const TripSchema = new mongoose.Schema({
@@ -239,14 +240,14 @@ app.post("/login", async (req, res) => {
         avatarImage: user.avatarImage,
       },
       jwtSecret,
-      { expiresIn: "1h" },
+      { expiresIn: "1day" },
       (err: Error | null, token: string | undefined) => {
         if (err) {
           console.error("JWT sign error:", err);
           return res.status(500).send("Failed to sign token");
         }
-        res.cookie("token", token, { httpOnly: true }).json(user);
-        res.status(200).send("Login correct and token set");
+        res.cookie("token", token, { httpOnly: true });
+        res.status(200).json(user);
       }
     );
   }
@@ -264,26 +265,42 @@ app.get("/places/:id", async (req, res) => {
       { $match: { _id: new mongoose.Types.ObjectId(id) } },
       {
         $lookup: {
-          from: "comments", // Ensure this matches the name of your comments collection
+          from: "comments",
           localField: "comments",
           foreignField: "_id",
           as: "comments",
-        },
-      },
-
-      {
-        $lookup: {
-          from: "photos", // Ensure this matches the name of your photos collection
-          localField: "photos",
-          foreignField: "_id",
-          as: "photos",
         },
       },
       {
         $unwind: { path: "$comments", preserveNullAndEmptyArrays: true },
       },
       {
-        $sort: { "comments.score": -1 },
+        $lookup: {
+          from: "users",
+          localField: "comments.userID",
+          foreignField: "_id",
+          as: "comments.userDetails",
+        },
+      },
+      {
+        $unwind: {
+          path: "$comments.userDetails",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      {
+        $addFields: {
+          "comments.username": "$comments.userDetails.username",
+          "comments.rank": "$comments.userDetails.rank",
+          "comments.rankImage": "$comments.userDetails.rankImage",
+          "comments.avatarImage": "$comments.userDetails.avatarImage",
+          "comments.contribution": "$comments.userDetails.contribution",
+        },
+      },
+      {
+        $project: {
+          "comments.userDetails": 0,
+        },
       },
       {
         $group: {
@@ -293,23 +310,42 @@ app.get("/places/:id", async (req, res) => {
         },
       },
       {
-        $replaceRoot: {
-          newRoot: {
-            $mergeObjects: ["$root", "$$ROOT"],
+        $addFields: {
+          "root.rate": { $avg: "$comments.rate" },
+          "root.totalComments": { $size: "$comments" },
+          "root.subRatings": {
+            $switch: {
+              branches: [
+                {
+                  case: { $eq: ["$root.category", "thingsToDo"] },
+                  then: {
+                    locationRate: { $avg: "$comments.location" },
+                    safety: { $avg: "$comments.safety" },
+                    facilities: { $avg: "$comments.facilities" },
+                    convenience: { $avg: "$comments.convenience" },
+                    staff: { $avg: "$comments.staff" },
+                  },
+                },
+                {
+                  case: { $eq: ["$root.category", "thingsToEat"] },
+                  then: {
+                    foodQuality: { $avg: "$comments.foodQuality" },
+                    valueForMoney: { $avg: "$comments.valueForMoney" },
+                    service: { $avg: "$comments.service" },
+                    menuVariety: { $avg: "$comments.menuVariety" },
+                    ambiance: { $avg: "$comments.ambiance" },
+                  },
+                },
+              ],
+              default: {
+                location: { $avg: "$comments.location" },
+                service: { $avg: "$comments.service" },
+                facilities: { $avg: "$comments.facilities" },
+                roomQuality: { $avg: "$comments.roomQuality" },
+                cleanliness: { $avg: "$comments.cleanliness" },
+              },
+            },
           },
-        },
-      },
-      {
-        $unwind: { path: "$photos", preserveNullAndEmptyArrays: true },
-      },
-      {
-        $sort: { "photos.score": -1 },
-      },
-      {
-        $group: {
-          _id: "$_id",
-          root: { $mergeObjects: "$$ROOT" },
-          photos: { $push: "$photos" },
         },
       },
       {
@@ -324,6 +360,7 @@ app.get("/places/:id", async (req, res) => {
     if (results.length === 0) {
       return res.status(404).json({ message: "Place not found" });
     } else {
+      console.log("Place found", results[0]);
       return res.json(results[0]); // Since we are doing a findOne equivalent
     }
   } catch (err) {
@@ -348,6 +385,134 @@ app.get("/profile", async (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.clearCookie("token").send("Logged out");
+});
+
+app.post("/setComment", async (req, res) => {
+  const placeInfo = await Place.findById(req.body.placeID);
+
+  if (placeInfo?.category === "thingsToDo") {
+    const {
+      placeID,
+      userID,
+      rate,
+      title,
+      commentBody,
+      dateVisit,
+      location,
+      convenience,
+      safety,
+      facilities,
+      staff,
+      writtenDate,
+      withWhom,
+    } = req.body;
+    const comment = new Comment({
+      placeID,
+      userID,
+      rate,
+      title,
+      commentBody,
+      dateVisit,
+      location,
+      convenience,
+      safety,
+      facilities,
+      staff,
+      writtenDate,
+      withWhom,
+    });
+
+    await comment.save().then((comment) => {
+      const commentID = comment._id;
+      Place.findByIdAndUpdate(
+        placeID,
+        { $push: { comments: commentID } },
+        { new: true }
+      ).then((place) => {
+        console.log("Comment saved in place", place);
+      });
+      User.findByIdAndUpdate(
+        userID,
+        { $push: { reviewComments: commentID } },
+        { new: true }
+      ).then((user) => {
+        console.log("Comment saved in user", user);
+      });
+    });
+  } else if (placeInfo?.category === "thingsToEat") {
+    const {
+      placeID,
+      userID,
+      rate,
+      title,
+      commentBody,
+      dateVisit,
+      service,
+      foodQuality,
+      valueForMoney,
+      menuVariety,
+      ambiance,
+
+      writtenDate,
+      withWhom,
+    } = req.body;
+    const comment = new Comment({
+      placeID,
+      userID,
+      rate,
+      title,
+      commentBody,
+      dateVisit,
+      service,
+      foodQuality,
+      valueForMoney,
+      menuVariety,
+      ambiance,
+
+      writtenDate,
+      withWhom,
+    });
+    await comment.save().then((comment) => {
+      console.log("Comment saved", comment);
+    });
+  } else {
+    const {
+      placeID,
+      userID,
+      rate,
+      title,
+      commentBody,
+      dateVisit,
+      location,
+      service,
+      facilities,
+      roomQuality,
+      cleanliness,
+      writtenDate,
+      withWhom,
+    } = req.body;
+    const comment = new Comment({
+      placeID,
+      userID,
+      rate,
+      title,
+      commentBody,
+      dateVisit,
+      location,
+      service,
+      facilities,
+      roomQuality,
+      cleanliness,
+      writtenDate,
+      withWhom,
+    });
+
+    await comment.save().then((comment) => {
+      console.log("Comment saved", comment);
+    });
+  }
+
+  res.send("trying to save the comment");
 });
 
 app.listen(4000, () => console.log("App listening on port 4000!"));
